24e375ac53652c6f494014aff795d568734930f4
Tobias Wallura <tobias@wallura.org>
2019-05-02 22:35:12 +0200

    allow retrieval of supported languages

--- src/country-data.ts
+++ src/country-data.ts
@@ -1,7 +1,12 @@
 import { getCountries, ICountry } from "./countries";
+import { data } from "./data";
 import { getRegions, IRegion } from "./regions";
 
 export class CountryData {
+  public static getSupportedLanguages(): string[] {
+    return Object.keys(data.i18n);
+  }
+
   public constructor(private readonly lang: string) {
     this.lang = lang.substring(0, 2);
   }
